fcddb0a67c604a62ac9fcb39a6b2ed06fd8c02ea
IsabelaToso <40370614+IsabelaToso@users.noreply.github.com>
2021-01-31 08:43:33 -0300

    Configurando bd e Flask

---
+++ db_config.py
@@ -0,0 +1,11 @@
+from app import app
+from flaskext.mysql import mysql
+
+mysql = MySQL()
+
+#MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'isa24337289'
+app.config['MYSQL_DATABASE_DB'] = 'desafiomaistodos'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
