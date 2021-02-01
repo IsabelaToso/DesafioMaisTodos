fe12fc35d2a29ca7ad35f85a0c78100515992a8a
IsabelaToso <40370614+IsabelaToso@users.noreply.github.com>
2021-02-01 10:33:10 -0300

    Configuração bd

--- db_config.py
+++ db_config.py
@@ -1,11 +1,10 @@
 from app import app
-from flaskext.mysql import mysql
+from flaskext.mysql import MySQL
 
-mysql = MySQL()
-
-#MySQL configurations
+# MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'isa24337289'
 app.config['MYSQL_DATABASE_DB'] = 'desafiomaistodos'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+
+mysql = MySQL(app)
