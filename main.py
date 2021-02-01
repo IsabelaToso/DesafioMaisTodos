a0286da16d879f8353738d74ea32afd639c8a1c6
IsabelaToso <40370614+IsabelaToso@users.noreply.github.com>
2021-02-01 18:01:04 -0300

    Main()
    Registrar um novo cartão de crédito completo.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import app
 from db_config import mysql
 from flask import request
+from function import validate_date, validate_holder, validate_number, validate_cvv, encrypt_decrypt
 
 @app.route('/RegisterNewCreditCard', methods=['POST'])
 def index():
@@ -10,15 +11,20 @@ def index():
         _number = _json[0]['number']
         _cvv = _json[0]['cvv']
 
-        sql = "INSERT INTO creditcard (exp_date, holder, number, cvv) VALUES (%s, %s, %s, %s);"
-        data = (_exp_date, _holder, _number, _cvv)
 
-        conn = mysql.connect()
-        myCursor = conn.cursor()
-        myCursor.execute(sql, data)
-        conn.commit()
-        conn.close()
-        
+        if validate_date(_exp_date)[0] == True and validate_holder(_holder) == True and validate_number(_number) == True and validate_cvv(_cvv) == True:
+           fullDate = validate_date(_exp_date)[1]
+           encryptedNumber = encrypt_decrypt(_number)
+
+           sql = "INSERT INTO creditcard (exp_date, holder, number, cvv) VALUES (%s, %s, %s, %s);"
+           data = (fullDate , _holder, encryptedNumber, _cvv)
+
+           conn = mysql.connect()
+           myCursor = conn.cursor()
+           myCursor.execute(sql, data)
+           conn.commit()
+           conn.close()
+
         return 'Credit card successfully registered!'
 
 if __name__ == "__main__":
