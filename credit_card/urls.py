dd9be8decbaa95756da303471fabd0724ab90260
IsabelaToso <40370614+IsabelaToso@users.noreply.github.com>
2021-02-07 20:43:31 -0300

    Atualizando funções register_user e login_user

--- credit_card/urls.py
+++ credit_card/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from credit_card import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    url(r'^api/credit_card/register_new$', views.register_new_credit_card, name= 'register_credit_card'),
-    url(r'^api/credit_card/detail/(?P<pk>[0-9]+)$', views.credit_card_detail),
+    url(r'^api/credit_card/register_new$', views.register_new_credit_card),
+    url(r'^api/credit_card/detail/(?P<number>[0-9]+)$', views.credit_card_detail),
     url(r'^api/credit_card/display_all$', views.all_credit_card),
     url(r'^api/credit_card/register_user$', views.register_user),
-    url(r'^api/credit_card/login$', views.user_login, name= 'login'),
+    url(r'^api/credit_card/login$', obtain_auth_token)
 ]
--- credit_card/views.py
+++ credit_card/views.py
@@ -1,72 +1,70 @@
-from django.shortcuts import render
-
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 from credit_card.models import CreditCard
-from credit_card.serializers import CreditCardSerializer
+from credit_card.serializers import CreditCardSerializer, RegisterSerializer
 from rest_framework.decorators import api_view
 from credit_card.functions import validate_date, validate_holder, validate_number, validate_cvv
 
+#registrar novo usuário
+@api_view(['POST'])
 def register_user(request):
     if request.method == "POST":
-        form_user = UserCreationForm(request.POST)
-        if form_user.is_valid():
-            form_user.save()
-            return redirect('register_credit_card')
-    else:
-        form_user = UserCreationForm()
-    return render(request, 'register.html', {'form_user': form_user})
+        user_data = JSONParser().parse(request)
+        user_serializer = RegisterSerializer(data=user_data)
+        if user_serializer.is_valid(raise_exception=True):
+            try:
+                user = User.objects.create_user(user_data['username'], user_data['email'], user_data['password'])
+                Token.objects.create(user=user)
+                return JsonResponse(user_serializer.data, status=status.HTTP_200_OK, safe=False)
+            except:
+                return JsonResponse(user_serializer.data, status=status.HTTP_400_BAD_REQUEST, safe=False)
 
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('register_credit_card')
-        else:
-            form_login = AuthenticationForm()
-    else:
-        form_login = AuthenticationForm()
-    return render(request, 'login.html', {'form_login': form_login})
-
-@api_view(['GET', 'POST', 'DELETE'])
+#registrar novo cartão de crédito
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([AllowAny])
 def register_new_credit_card(request):
     if request.method == 'POST':
-
-            credit_card_data = JSONParser().parse(request)
-            validDate, newDate = validate_date(credit_card_data['exp_date'])
-            validNumber, brand = validate_number(credit_card_data['number'])
-            credit_card_data['exp_date'] = newDate
-            credit_card_data['brand'] = brand
-            print(credit_card_data['brand'])
-
-            if validDate == True and validate_holder(credit_card_data['holder']) == True and validNumber == True and validate_cvv(credit_card_data['cvv']) == True:
+        credit_card_data = JSONParser().parse(request)
+        try:
+            if validate_date(credit_card_data['exp_date'])[0] == True and validate_holder(credit_card_data['holder']) == True and validate_number(credit_card_data['number'])[0] == True and validate_cvv(credit_card_data['cvv']) == True:
+                credit_card_data['exp_date'] = validate_date(credit_card_data['exp_date'])[1] #substitui pela data no formato correto
+                credit_card_data['brand'] = validate_number(credit_card_data['number'])[1] #adiciona a brand do cartão
                 credit_card_serializer = CreditCardSerializer(data=credit_card_data)
-                if credit_card_serializer.is_valid():
+                if credit_card_serializer.is_valid(raise_exception=True):
                     credit_card_serializer.save()
                     return JsonResponse(credit_card_serializer.data, status=status.HTTP_201_CREATED)
+            else:
                 return JsonResponse(credit_card_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return JsonResponse({'mensagem': 'Invalid Parameters'}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def credit_card_detail(request, pk):
-    try:
-        credit_card_data = CreditCard.objects.get(number=pk)
-        if request.method == 'GET':
-            credit_card_serializer = CreditCardSerializer(credit_card_data)
-            return JsonResponse(credit_card_serializer.data)
-    except CreditCard.DoesNotExist:
-        return JsonResponse({'message': 'This credit card does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#obter detalhes de um único cartão de crédito
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([AllowAny])
+def credit_card_detail(request, number):
+        try:
+            if request.method == 'GET':
+                credit_card_data = CreditCard.objects.get(number=number)
+                credit_card_serializer = CreditCardSerializer(credit_card_data)
+                return JsonResponse(credit_card_serializer.data, status=status.HTTP_200_OK)
+        except CreditCard.DoesNotExist:
+            return JsonResponse({'message': 'Credit card not found!'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'PUT', 'DELETE'])
+#obter detalhes de todos os cartões de crédito
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([AllowAny])
 def all_credit_card(request):
     if request.method == 'GET':
         credit_card_data = CreditCard.objects.all()
         credit_card_serializer = CreditCardSerializer(credit_card_data, many=True)
-        return JsonResponse(credit_card_serializer.data, safe=False)
+        return JsonResponse(credit_card_serializer.data, status=status.HTTP_200_OK, safe=False)
